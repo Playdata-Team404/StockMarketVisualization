efdc40cc5c6fb3309e4696cd568390b50ab70650
Henry-choi426 <84162474+Henry-choi426@users.noreply.github.com>
2021-07-16 16:13:18 +0900

    뉴스 크롤링

--- crawler.py
+++ crawler.py
@@ -81,6 +81,56 @@ class Crawling():
         df
         df.to_csv("C:/ELKStack/0.dataset/stock.csv",header=False,index=False)
   
+    def crawl_news_all():
+        driver = webdriver.Chrome("C:/driver/chromedriver")
+        stock=[]
+        driver.get('https://m.stock.naver.com/index.html#/domestic/capitalization/KOSPI')
+        for i in range(1,11):
+
+            #각 회사 들어가기
+            driver.find_element_by_xpath('//*[@id="content"]/div[4]/div[2]/div[2]/div[1]/table/tbody/tr[{}]'.format(i)).click()
+            time.sleep(1)
+
+            #뉴스항목 들어가기
+            driver.find_element_by_xpath('//*[@id="common_component_tab"]/div/ul/li[3]/a/span').click()
+            time.sleep(1)
+            
+            # 뉴스 클릭
+            
+            for i in range(1,21):
+
+                time.sleep(3)
+                news=[]
+                
+                try:
+                    driver.find_element_by_xpath('//*[@id="content"]/div[4]/div[3]/div[2]/div/div[4]/ul/li[{}]/a'.format(i)).click()
+                except:
+                    print("Pass")
+                    continue
+
+                time.sleep(2)
+
+                news.append(driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div[1]/span[1]').text[:6])
+                news.append(driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[1]/div[1]/span[2]').text)
+                news.append(driver.find_element_by_xpath('//*[@id="content"]/div[4]/div[3]/div[2]/div/div[1]/div[1]/div[1]/time').text[:10])
+                news.append(driver.find_element_by_xpath('//*[@id="content"]/div[4]/div[3]/div[2]/div/div[1]/div[1]/strong').text)
+                news.append(driver.find_element_by_xpath('//*[@id="content"]/div[4]/div[3]/div[2]/div/div[1]/div[2]/div[1]').text)
+
+                time.sleep(2)
+                driver.back()
+                time.sleep(2)
+                stock.append(news)
+                
+            driver.back()
+            time.sleep(2)
+            driver.back()
+            time.sleep(2)
+
+        col=['code','name','date','subject','content']
+        df = pd.DataFrame(stock,columns=col)
+        df.to_csv("news.csv", mode='a',header=False,index=False)
+
 if __name__ == '__main__':
     # Crawling.crawl_stock_all()
-    Crawling.crawl_stock_one()
+    # Crawling.crawl_stock_one()
+    Crawling.crawl_news_all()
